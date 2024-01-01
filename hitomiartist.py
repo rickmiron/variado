67241567b434b566a02d0d7c44de5ba8bb1e2406
rickmiron <99231649+rickmiron@users.noreply.github.com>
2024-01-01 07:20:17 -0500

    Add files via upload

---
+++ hitomiartist.py
@@ -0,0 +1,59 @@
+import requests
+import struct
+import constants
+
+class InDig(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle(" ")
+
+        self.infd = QLineEdit(self)
+        self.okbu = QPushButton("OK", self)
+        self.cabu = QPushButton("Cancel", self)
+
+        lay = QVBoxLayout(self)
+        lay.addWidget(self.infd)
+        lay.addWidget(self.okbu)
+        lay.addWidget(self.cabu)
+
+        pal = self.infd.palette()
+        pal.setColor(QPalette.ColorRole.Text, QColor("red"))
+        pal.setColor(QPalette.ColorRole.ButtonText, QColor("red"))
+        self.infd.setPalette(pal)
+        self.okbu.setPalette(pal)
+        self.cabu.setPalette(pal)
+
+        self.okbu.clicked.connect(self.accept)
+        self.cabu.clicked.connect(self.reject)
+
+    def getex(self):
+        return self.infd.text()
+
+def showInDig():
+    dig = InDig(None)
+    if dig.exec() == 1:
+        tup = f_noserch(dig.getex())
+        ui.edit.setText(str(tup)[1:-1])
+        ui.downButton.click()
+
+def f_noserch(url):
+    url = url.replace("//hitomi.", "//ltn.hitomi.")
+    url = url[:url.rfind(".html")] + ".nozomi"
+    res = requests.get(url)
+    if res.status_code in [200, 206]:
+        arbuf = res.content
+        if arbuf:
+            data = memoryview(arbuf)
+            return struct.unpack_from(">"+"i"*(len(data) // 4), data)
+
+ficon = QtGui.QIcon()
+ficon.addPixmap(
+    QtGui.QPixmap("imgs/icons/disk.png")
+)
+
+ui.action_fread4 = QtGui.QAction(constants.mainWindow)
+ui.action_fread4.setIcon(ficon)
+ui.action_fread4.setObjectName("action_fread4")
+ui.action_fread4.triggered.connect(showInDig)
+ui.action_fread4.setText("Hitomi artist")
+ui.menu_2.addAction(ui.action_fread4)
