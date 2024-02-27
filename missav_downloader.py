a5561888d8913b06b9ffa6d4ebef7f8aeff8473a
rickmiron <99231649+rickmiron@users.noreply.github.com>
2024-02-26 20:54:23 -0500

    Update missav_downloader.py

--- missav_downloader.py
+++ missav_downloader.py
@@ -1,6 +1,6 @@
 #coding:utf8
 #title_en: Missav
-#https://missav.com/
+#comment: https://missav.com/
 #author: Rickelpapu
 
 import downloader
@@ -20,9 +20,10 @@ class Video:
         self.dir = dir
         get_print(self.cw)('Get ofuscado')
         urlv = self.ofuscado(url)
+        get_print(self.cw)(urlv)
         ms = [
-            lambda: playlist2stream(urlv,referer='https://missav.com', n_thread=8),
-            lambda: M3u8_stream(urlv,referer='https://missav.com', n_thread=8),
+            lambda: playlist2stream(urlv,referer='https://missav.com', n_thread=1),
+            lambda: M3u8_stream(urlv,referer='https://missav.com', n_thread=1)
             ]
         for m in ms:
             try:
@@ -37,6 +38,7 @@ class Video:
         self.thumb = BytesIO()
         self.thumbz = BytesIO()
         self.cw.setTitle('{}...{}'.format(tr_('썸네일 다운로드'), self.filename))
+        get_print(self.cw)(self.urlthumb)
         downloader.download(self.urlthumb, buffer=self.thumbz)
         self.dirfile = '{}\\{}'.format(dir, self.filename)
         self.tojpg()
@@ -86,22 +88,16 @@ class Video:
         codigo = soup.findAll('script', {'type': 'text/javascript'})[2].text.strip()
         un = codigo.find('eval(')
         codigo = codigo[un:codigo.find('.split(',un) - 1]
-        un = 0
-        for _ in range(3):
-            un = codigo.find('://',un + 1)
-        inpu = codigo[un-1:codigo.find(';',un)-2]
+        un = codigo.find('://')
         k_array = codigo[codigo.find(',\'',un) + 2:].split('|')
         narray = []
-        for car in inpu:
+        for car in codigo[un-1:codigo.find(';',un)-2]:
             num = ord(car)
-            if 47 < num < 58:
-                ee = k_array[num - 48] or car
-            elif 96 < num < 123:
-                ee = k_array[num - 87] or car
-            else:
-                ee = car
-            narray.append(ee)
-        return ''.join(narray)
+            narray.append((k_array[num - 48] if 58 > num > 47 else k_array[num - 87] if 96 < num < 123 else car) or car)
+        url = ''.join(narray)
+        codigo = downloader.read_html(url,user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36')
+        un = codigo.find('\n',codigo.rfind('INF:'))+1
+        return url.replace('playlist.m3u8',codigo[un:codigo.find('\n',un)])
     
     def pp(self, filename):
         self.thumb.seek(0)
@@ -121,8 +117,9 @@ class Downloader_missav(Downloader):
     strip_header = False
     URLS = ['missav.com']
     display_name = 'Missav'
-    MAX_CORE = 8
+    MAX_CORE = 2
     MAX_PARALLEL = 2
+    MAX_SPEED = 0.7
     icon = 'base64:AAABAAEAICAAAAEAIAAoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAABAAAAAAAAAAAAAAEAAAABAAEAgGPjEodi7luLYvmeimL2zYpi9+qNYfz+jWH8/opi9uqKY/bNi2L5nodi71uAY+MSAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAEAAAAAAAAAgGDsKIhi9Z6MYvr0jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OY/7/jmL+/4xi+vSJYvWegGDsKAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgWrVDIhf9I6MYvz4jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmP+/45i/v+MYvz4iV/1joBq1AwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAINk7CmKYfnTjmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+PYv7/jmL+/45i/v+OYv7/jmL+/49i/v+OYv7/jmL+/45i/v+OYv7/imH404Nk7CkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCZOszjGH66Y5i/v+OY/7/jmL+/45i/v+PYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i//+OYv7/jmL+/45j/v+PYv7/jGH66YJk6zMAAQAAAAAAAAAAAAAAAQAAAAABAAAAAAAAAAAAg2TsKYxh+umOYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45j//+OYv7/jmL+/45i/v+PYv7/jmL+/45i/v+PYv7/jmL+/45j//+OYv7/jmL+/45i//+PYv7/jGH66YNk7CkAAAEAAAABAAAAAAAAAAAAAAABAIBq1AyKYfjTjmP+/45i/v+OYv7/jmL+/45j/v+OYv//jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45j/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/imH404Br1AwAAAAAAAAAAAAAAAAAAAAAiF/0jo5i/v+OYv7/jmL+/49i/v+OYv7/jmL+/45i//+OYv7/jmL//49i/v+PYv7/j2L+/45i/v+OYv7/jmP+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i//+OYv7/j2L+/45i/v+OYv7/iF/0jgABAAAAAQAAAAAAAIBh7CiMYvz4j2L+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv//jmL+/45i/v+OYv7/jmL+/45j/v+OYv7/jmL//45i/v+OYv7/jmL+/45i/v+OY/7/jmL+/45i//+OYv//j2L+/49i/v+MYvz4gGDsKAAAAAAAAAAAiWL0no5i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/597/f/g1vv/zLn9/5Zv/P+OYv7/jmL//45i/v+OYv7/jmP+/45j/v+PYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+JYvWeAAAAAIBj4xKNYvr0jmL+/45j//+OYv7/jmL+/45i/v+OYv7/jmP+/45i/v+OYv7/xrL4///+////////9fL+/7qh/P+QZP3/jmL+/49i/v+OYv7/jmL+/45j/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/4xj+vSAY+MSh2LuW45j/v+OYv//jmL+/45i/v+OYv7/jmL//45i//+OYv7/jmL+/45i/v/Itvj////+///////+/////////+be/P+oiPz/jmL+/45i/v+OYv7/j2L+/45i/v+OYv7/jmL+/45i/v+PYv7/jmL+/45i/v+OY/7/jmL+/4di7luLYvmejmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmP+/8i2+P////////////////////7///////39///Tw/z/mHL7/45i/v+OYv7/jmL+/49i/v+OYv7/jmL+/45i/v+PYv7/jmL+/45i/v+OYv//i2L4nopi9s2OYv7/j2L+/49i/v+OY/7/jmL+/45i/v+OYv7/j2L+/45i/v+OYv7/yLb4//7///////////////////////////////7////39f7/v6j6/5Fm/v+PYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45j/v+KYvbNimL36o5i/v+OYv7/jmL+/49i/v+OYv7/jmL+/45i//+OYv7/jmL+/45i/v/Itvj//v///////////////v////7//v//////////////////////6uL9/6uM/P+OYv7/jmL+/45i/v+OYv7/j2L+/45j/v+OYv7/jmL+/4pi9+qNYfz+jmL+/45i/v+OYv7/jmP+/45i/v+OYv7/jmL+/45j/v+OYv//jmL+/8i2+P////////7///7//v////////////////////////7//////////////v7//6aH+/+OY/7/jmL+/49i/v+OYv7/jmL+/45j/v+OYv7/jWH8/o1h/P6OYv7/j2L+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/yLb5/////////////v////////////////////7//////////v/////////9/f7/pIP6/45i/v+OYv7/jmP+/45i/v+OYv7/jmL+/45i/v+NYfz+imL36o5i/v+OY/7/jmL//45i//+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v/Itvj/////////////////////////////////////////////////5t/8/6eH+v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/4pi9+qKYvbNjmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/49i/v+OYv//jmL+/8m2+P///v/////////////+////////////////////9fL9/7uj+/+QZf7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/imP3zYti+Z6OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/yLb4/////////////////////////v///f3+/9HA/P+Wbv3/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmP+/45i//+OYv7/jmL+/49i//+LYvmeh2PuW45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv//j2L+/45i/v/Itvj//////////////////////+Pa/f+igPv/jmL+/45i/v+OYv7/jmL+/45i//+OYv7/jmL+/45i/v+OYv//jmL//45i/v+OY/7/jmL+/4di7luAY+MSjGL69I5i/v+OY/7/jmL+/45i/v+OYv//jmP+/45i/v+OYv7/jmP+/8ey+f////////////Pv/f+3nPz/jmP+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45j/v+OY/7/jmL+/45i/v+OYv7/jmL+/45i/v+MYvr0gGPjEgAAAACJY/WejmL//45i/v+OYv7/jmL+/45i/v+OYv7/j2L+/45i/v+OYv7/n3n9/9/T/v/ItPz/k2r8/45i/v+OYv7/j2L+/45i/v+OYv7/jmL+/45j/v+OYv7/j2P+/45j/v+OYv7/jmL+/49i/v+OYv7/jmL+/4li9Z4AAAEAAAAAAIBg7CiMY/z4jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/j2L+/45i/v+OYv7/jmL+/45i/v+OYv//jmL+/45i/v+OYv7/j2L+/45i/v+OYv7/jmL+/45i//+OYv7/jmP+/45j/v+OYv7/jmL+/45i/v+MYvz4gGDsKAAAAAAAAAAAAAEAAIhf9I6OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45j/v+OYv7/jmL+/45j/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/4hf9Y4AAAAAAAAAAAAAAAAAAAAAgGrVDIph+NOOYv7/jmL//45i/v+OYv7/jmL+/45i//+OYv7/jmL+/45i/v+OYv7/jmL+/45j/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OY/7/j2L+/45i/v+OYv//jmL+/45i/v+KYfjTgGrUDAAAAAAAAQEAAAAAAAAAAQAAAAAAg2TsKYxh+umOYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45j/v+OYv7/jmL+/49i/v+OYv7/jmL+/45i/v+PYv//jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jGH66YNl7CkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgmTrM4xh+umOYv7/j2L+/45i/v+OYv//jmP+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL//49j//+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+PYv7/j2L+/41h+umDZOozAAAAAAAAAAAAAAAAAAAAAAAAAQABAAAAAAAAAAAAAQAAAQAAg2TsKYph+NOOYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i/v+OYv//jmL+/45i/v+OYv7/jmL+/49i/v+OYv7/jmL+/45i/v+KYfjTg2TsKQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgGrUDIhf9I6MY/z4jmL+/45i/v+OYv7/jmL+/45j/v+OYv7/jmL+/45i/v+OYv7/j2L+/45i/v+OYv7/jmL+/45i/v+MYvz4iF70joBq1AwAAAAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAAAAAQAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAIBg7CiJYvWejGL69I5i/v+OYv7/jmL+/45i/v+OYv7/jmL+/45i//+OYv7/jmL+/45i/v+MY/r0iWP1noBg7SgBAAAAAAAAAAABAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAY+MSh2LuW4ti+Z6KYvbNimL36o1h/P6MYfz+imL36opi9s2LYvmeh2LuW4Bj4xIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEBAAAAAAAAAAAA'
     ACCEPT_COOKIES = [r'(.*\.)?(missav)\.(com)']
 
