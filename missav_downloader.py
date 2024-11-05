d00bcbc1714de15c530d463b5db57efeecd3f0a0
rickmiron <99231649+rickmiron@users.noreply.github.com>
2024-11-04 23:27:03 -0500

    Update missav_downloader.py

--- missav_downloader.py
+++ missav_downloader.py
@@ -5,7 +5,6 @@
 import downloader
 from utils import Downloader,try_n,LazyUrl,get_print,Soup,clean_title,Session,get_resolution
 from translator import tr_
-from error_printer import print_error
 from m3u8_tools import M3u8_stream
 from io import BytesIO
 from PIL import Image
@@ -25,11 +24,7 @@ class Video:
         sesion.headers.clear()
         sesion.headers.update({'Origin':'https://missav.com','User-Agent':USERAGEN})
         if urlv[urlv.rfind('.'):]=='.m3u8':
-            m=lambda: M3u8_stream(urlv,session=sesion,n_thread=4)
-            try:
-                m=m()
-            except Exception as e:
-                raise e
+            m=Strimin(urlv,session=sesion,n_thread=4)
             if getattr(m,'live',None)is not None:
                 m=m.live
         else:
@@ -68,22 +63,19 @@ class Video:
             self.thumbz.truncate(0)
 
     def ofuscado(self,url):
-        try:
-            soup=Soup(downloader.read_html(url,user_agent=USERAGEN))
-        except Exception as e:
-            get_print(self.cw)(print_error(e))
-        self.urlthumb=soup.find('meta',{'property':'og:image'}).attrs['content']
+        soup=Soup(downloader.read_html(url,user_agent=USERAGEN))
+        self.urlthumb=soup.find('meta',{'property':'og:image'})['content']
         title=soup.find('h1').text.strip()
         vid='/video/'not in url
         if vid:
-            codigo=soup.find('meta',{'property':'og:url'}).attrs['content']
+            codigo=soup.find('meta',{'property':'og:url'})['content']
             codigo=codigo[codigo.rfind('/')+1:].upper()
             un=title.find(' ')
             if un==-1:
                 un=len(title)
             title=codigo+title[un:]
         self.filename=clean_title(title,n=190)+'.mp4'
-        codigo=soup.findAll('script',{'type':'text/javascript'})[-2].text.strip()
+        codigo=soup.findAll('script',{'type':'text/javascript'})[-2].text
         un=codigo.find('eval(')
         codigo=codigo[un:codigo.find('.split(',un)-1]
         un=codigo.find('://')
@@ -119,6 +111,10 @@ class Video:
             remove(pathum)
         return filename
 
+class Strimin(M3u8_stream):
+    def _pp(self,_):
+        pass
+
 class Downloader_missav(Downloader):
     type = 'missav'
     single=True
@@ -126,6 +122,7 @@ class Downloader_missav(Downloader):
     URLS=['missav.com']
     display_name='Missav'
     MAX_PARALLEL=2
+    #MAX_SPEED=0.7
     icon='base64:iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAaVBMVEX+Yo4AAAD/Yo7+Y47////+Yo//Yo////7+Y4/mvcbvYof/AADQWXHnYoD7YYyqVVX2YYn+Yo3+/////v//Y46ZMzP//f7mu8X9aZL//Pz9Z5H5ep31xdH52uLziqX9h6jxl6/2ztj73uab0JJXAAAAI3RSTlP/AP//////////zgErp/wD3/3///8F/////////////////8G0tiwAAAGzSURBVHicZVOLcoMwDDNxAi1QCpSytnvv/z9ykp3Qdssd4S6WZUV2pLqvfhw0qAxj/3C4Abq2lqQhiEgt0nZ/AQcEhNGUBCxJDk+A46QM4mMwIa4yHe+As5KWEEGENIY+F8CRZ/XO4k4TsANEDgIm5i4WpABwkAbf5ADoo6pcArvKzilqKJWq80iAisX2rAN7CB0AbUkTXd8sYGZ4lRYA2GOqRV7iHF910Z2og3BcSe/BANg+xni6+m38VEIvY740FgGxie/FVMJGGTRrEgWgAUeMH9kSrMF/yVTuG0ZjnJv1JlbJU1ki4XoKgMXJc3GRLijUC+4GDU0uEZvGAOZvMQX/PePGsV6srt1/kFTatLfk+RS/TFLNqwwyOpeJnC39B41gS1lcRxoFdiPBNed4veUEzA1WL1Wo2WM0GlY3p8/NAc4Vra5aaHHjdP2+O+SLzeowTRTFD03K9hj/ztrNgUloFYPBg9l74WjbyCHgQ4s8uO6dXOoyctWRKsrCHJHGQGVoq3Pufja3SNjGng/HszeJqPP4cPj0qJN5GGijeH569nhtKlCBGv4/3vz84bs+P/9fOZAMztosBq4AAAAASUVORK5CYII='
     ACCEPT_COOKIES=[r'(.*\.)?(missav)\.(com)']
 
